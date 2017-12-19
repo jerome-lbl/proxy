86c05e4d378765f511e47b33389709be45d5f6b3
jerome-lbl <jerome.helbling@gmail.com>
2017-12-19 21:54:22 +0100

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -9,7 +9,7 @@ echo ${ip:0:6}
 if [ "${ip:0:6}" = "172.31" ]
 then
 	echo "Réseau local"
-	ssh -L ${HOSTNAME}:3129:proxyng.in.ac-versailles.fr:8080 -CN ngmdev@groli3.in.ac-versailles.fr
+	ssh -L ${HOSTNAME}:3129:hostname-proxy:8080 -CN ngmdev@hostname-gateway
 else
 	echo "Réseau internet"
 fi
